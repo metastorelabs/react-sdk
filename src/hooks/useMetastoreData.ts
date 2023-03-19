ce13548e58c0ebc612b054d84bf4fa833c25e783
santhosh <august.vs.sant@gmail.com>
2023-03-19 09:45:12 +0530

    Changed useMetastoreData hook to wait for loaded message

--- src/hooks/useMetastoreData.ts
+++ src/hooks/useMetastoreData.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 type MetastoreData = {
   isMobile: boolean
@@ -9,17 +9,25 @@ function useMetastoreData(): [MetastoreData | null, boolean] {
   const [data, setData] = useState<MetastoreData | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
 
-  window.addEventListener(
-    'message',
-    (event) => {
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
       if (event.origin !== 'https://meta-store.in') return
+      const { message, data } = event.data
 
-      const { data } = event
-      setData(data)
-      setLoading(false)
-    },
-    false,
-  )
+      if (message === 'loaded') {
+        window.parent.postMessage('getMetastoreData', 'https://meta-store.in')
+      } else if (message === 'metastoreData') {
+        setData(data)
+        setLoading(false)
+      }
+    }
+
+    window.addEventListener('message', handleMessage, false)
+
+    return () => {
+      window.removeEventListener('message', handleMessage, false)
+    }
+  }, [])
 
   return [data, loading]
 }
