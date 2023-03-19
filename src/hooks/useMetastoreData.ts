aaf5c5ea1d04729dba9ef89ad1421e7f2e8e707a
santhosh <august.vs.sant@gmail.com>
2023-03-19 13:11:36 +0530

    Returning isMetastore state from useMetastoreData

--- src/hooks/useMetastoreData.ts
+++ src/hooks/useMetastoreData.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { useState, useEffect } from 'react'
 
 type MetastoreData = {
@@ -5,31 +6,43 @@ type MetastoreData = {
   orientation: 'portrait' | 'landscape'
 }
 
-function useMetastoreData(): [MetastoreData | null, boolean] {
+function useMetastoreData(): { data: MetastoreData | null; loading: boolean; isMetastore: boolean } {
   const [data, setData] = useState<MetastoreData | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
+  const [isMetastore, setIsMetastore] = useState<boolean>(false)
 
   useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.origin !== 'https://meta-store.in') return
-      const { message, data } = event.data
+    const inIframe = window.self !== window.top
+    const referrer = document.referrer
+    const isMetaStore = referrer.includes('https://meta-store.in') && inIframe
 
-      if (message === 'loaded') {
-        window.parent.postMessage('getMetastoreData', 'https://meta-store.in')
-      } else if (message === 'metastoreData') {
-        setData(data)
-        setLoading(false)
+    setIsMetastore(isMetaStore)
+
+    if (isMetaStore) {
+      const handleMessage = (event: MessageEvent) => {
+        if (event.origin !== 'https://meta-store.in') return
+        const { message, data } = event.data
+
+        if (message === 'loaded') {
+          window.parent.postMessage('getMetastoreData', 'https://meta-store.in')
+        } else if (message === 'metastoreData') {
+          setData(data)
+          setLoading(false)
+        }
       }
-    }
 
-    window.addEventListener('message', handleMessage, false)
+      window.addEventListener('message', handleMessage, false)
 
-    return () => {
-      window.removeEventListener('message', handleMessage, false)
+      return () => {
+        window.removeEventListener('message', handleMessage, false)
+      }
+    } else {
+      setLoading(false)
+      return () => {}
     }
   }, [])
 
-  return [data, loading]
+  return { data, loading, isMetastore }
 }
 
 export default useMetastoreData
